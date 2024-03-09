4196e08f3c2c83eaf139ebb75c5a3251b9d8a914
Alireza Asakrpour <askarpourdev@gmail.com>
2024-03-10 01:37:16 +0330

    feat: added update label endpoint

--- src/modules/labels/labels.controller.ts
+++ src/modules/labels/labels.controller.ts
@@ -1,9 +1,17 @@
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { LabelsService } from './labels.service';
 import { AuthGuard } from 'src/core/guards/auth.guard';
 import { RequiredPublicSettingsGuard } from 'src/core/guards/public-setting.guard';
 import { CreateLabelDto } from './dtos/create-label.dto';
+import { UpdateLabelDto } from './dtos/update-label.dto';
 
 @ApiBearerAuth()
 @ApiTags('Labels')
@@ -13,7 +21,13 @@ export class LabelsController {
 
   @UseGuards(AuthGuard, RequiredPublicSettingsGuard)
   @Post()
-  createProperties(@Body() body: CreateLabelDto) {
+  createLabel(@Body() body: CreateLabelDto) {
     return this.labelsService.createLabel(body);
+  }
+
+  @UseGuards(AuthGuard, RequiredPublicSettingsGuard)
+  @Patch(':id')
+  updateLabel(@Param('id') id: string, @Body() body: UpdateLabelDto) {
+    return this.labelsService.updateLabel(id, body);
   }
 }
--- src/modules/labels/labels.service.ts
+++ src/modules/labels/labels.service.ts
@@ -6,6 +6,9 @@ import {
 } from '@nestjs/common';
 import { LabelsRepository } from './labels.repository';
 import { CreateLabelDto } from './dtos/create-label.dto';
+import { UpdateLabelDto } from './dtos/update-label.dto';
+import { ResponseFormat } from 'src/core/interfaces/response.interface';
+import { ResponseMessages } from 'src/core/constants/response-messages.constant';
 
 @Injectable()
 export class LabelsService {
@@ -29,6 +32,30 @@ export class LabelsService {
     return {
       statusCode: HttpStatus.CREATED,
       message: 'LABEL_CREATED_SUCCESS',
+    };
+  }
+
+  public async updateLabel(
+    _id: string,
+    body: UpdateLabelDto,
+  ): Promise<ResponseFormat<any>> {
+    // prevented duplicated slug
+    const duplicatedSlug = await this.labelsRepository.findOne({
+      slug: body.slug,
+    });
+
+    if (duplicatedSlug && duplicatedSlug._id.toString() !== _id) {
+      throw new ConflictException(ResponseMessages.PROPERTY_SLUG_ALREADY_EXIST);
+    }
+
+    const updatedResult = await this.labelsRepository.updateOne({ _id }, body);
+    if (updatedResult.modifiedCount !== 1) {
+      throw new InternalServerErrorException('FAILED_UPDATE_LABEL');
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'LABEL_UPDATED_SUCCESS',
     };
   }
 }
